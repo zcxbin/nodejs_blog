5ebf5ed074446af93527876c7606050ceb3b9d3c
zcxbin <zcxbin@gmail.com>
2024-06-21 16:03:50 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views')) // Corrected the directory name and used forward slashes
+
+// Route  
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+// Start server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
